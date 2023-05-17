27107880ba6f278ec046ed679a0cb7d6dbd297fc
Hassan ali <il.magnifico@icloud.com>
2023-05-17 14:11:43 +0300

    init

---
+++ WeatherApp.R
@@ -0,0 +1,31 @@
+library(shiny)
+library(httr)
+
+ui <- fluidPage(
+  titlePanel("Weather App"),
+  sidebarLayout(
+    sidebarPanel(
+      textInput("city_input", "Enter City Name:", value = ""),
+      br(),
+      actionButton("submit_button", "Submit")
+    ),
+    mainPanel(
+      textOutput("weather_output")
+    )
+  )
+)
+
+server <- function(input, output) {
+  observeEvent(input$submit_button, {
+    city <- URLencode(input$city_input)
+    apiKey <- '0981a53056535828bdc13e46f571d4a2'
+    url <- paste0("http://api.openweathermap.org/data/2.5/weather?q=", city, "&appid=", apiKey, "&units=metric")
+    response <- GET(url)
+    weather_data <- content(response, as = "parsed")
+    temp <- weather_data$main$temp
+    humidity <- weather_data$main$humidity
+    output$weather_output <- renderText(paste0("Temperature: ", temp, "°C\n", "Humidity: ", humidity, "%"))
+  })
+}
+
+shinyApp(ui = ui, server = server,options = list(host = ), port = 5002))
